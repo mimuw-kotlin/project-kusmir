d3fada3f760763c5a749d7058c39f1e0082d98ff
kusmir <michal@kusmirek.info>
2024-11-20 14:49:46 +0000

    Add Deck Entity to database schema

---
+++ src/test/kotlin/data/repository/CardsRepositoryFullDataTest.kt
@@ -0,0 +1,93 @@
+@file:OptIn(ExperimentalUuidApi::class)
+
+package data.repository
+
+import DatabaseDriverFactory
+import app.softwork.uuid.sqldelight.UuidByteArrayAdapter
+import app.softwork.uuid.toUuid
+import data.data_source.CardsDaoImpl
+import data.local.database.CardDb
+import data.local.database.Card_deck
+import data.local.database.Database
+import data.network.ScryfallApiImpl
+import data.sqldelight.CustomAdaptersImpl
+import domain.model.MtgColor
+import domain.repository.CardsRepository
+import kotlinx.coroutines.test.runTest
+import mock.ScryfallApiMock
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+import kotlin.time.DurationUnit
+import kotlin.time.toDuration
+import kotlin.uuid.ExperimentalUuidApi
+
+class CardsRepositoryFullDataTest {
+
+    companion object {
+        lateinit var cardsRepository: CardsRepository
+
+        @OptIn(ExperimentalUuidApi::class)
+        @BeforeAll
+        @JvmStatic
+        fun setup() = runTest(timeout = 5.toDuration(DurationUnit.MINUTES)) {
+            val driver = DatabaseDriverFactory().createDriver()
+            Database.Schema.create(driver)
+            val db = Database(
+                driver = driver,
+                cardDbAdapter = CardDb.Adapter(
+                    idAdapter = UuidByteArrayAdapter,
+                    colorsAdapter = CustomAdaptersImpl().ListStringAdapter(),
+                    legalitiesAdapter = CustomAdaptersImpl().legalitiesAdapter()
+                ),
+                card_deckAdapter = Card_deck.Adapter(
+                    cardIdAdapter = UuidByteArrayAdapter
+                )
+            )
+
+            val dao = CardsDaoImpl(db)
+            val scryfallApi = ScryfallApiMock("OracleCardsFull.json")
+
+            cardsRepository = CardsRepositoryImpl(
+                cardsDao = dao,
+                scryfallApi = scryfallApi
+            )
+
+            cardsRepository.fetchAndUpdateCardsData()
+        }
+    }
+
+    @OptIn(ExperimentalUuidApi::class)
+    @Test
+    fun testGetById() = runTest {
+        val id = "55e4b27e-5447-4fc2-8cae-a03e344600c6".toUuid()
+        val actualName = "Nicol Bolas, the Ravager // Nicol Bolas, the Arisen"
+
+        val card = cardsRepository.getCardById(id)
+
+        assertNotNull(card)
+        assertEquals(actualName, card.name)
+        assertEquals(id, card.id)
+        assertEquals(
+            setOf(MtgColor.BLUE, MtgColor.RED, MtgColor.BLACK),
+            card.colorIdentity
+        )
+    }
+
+    @OptIn(ExperimentalUuidApi::class)
+    @Test
+    fun testGetByNameSimple() = runTest {
+        val name = "Lightning Bolt"
+        val expectedId = "4457ed35-7c10-48c8-9776-456485fdf070".toUuid()
+
+        val card = cardsRepository.getCardByName(name)
+
+        assertNotNull(card)
+        assertEquals(name, card.name)
+        assertEquals(expectedId, card.id)
+        assertEquals(setOf(MtgColor.RED), card.colorIdentity)
+    }
+
+}
