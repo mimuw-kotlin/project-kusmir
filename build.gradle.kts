44247d60ba56d185640883dd3d12a5f00ed5f14a
kusmir <michal@kusmirek.info>
2025-02-03 05:51:09 +0100

    Move all dependencies to libs.versions.toml

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,9 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompilationTask
 
 plugins {
     kotlin("jvm") version "2.0.20"
-    id("org.jlleitschuh.gradle.ktlint") version "12.1.1"
-    id("org.jetbrains.compose") version "1.7.0-beta01"
-    id("org.jetbrains.kotlin.plugin.compose")
+    alias(libs.plugins.ktlint)
+    alias(libs.plugins.compose)
+    alias(libs.plugins.kotlin.compose.plugin)
     alias(libs.plugins.sqldelight)
     alias(libs.plugins.kotlinxSerialization)
 }
@@ -34,16 +34,17 @@ dependencies {
     implementation(libs.koin.core)
     implementation(libs.koin.compose.viewmodel)
     implementation(libs.navigation.compose)
-    implementation(libs.kotlinx.coroutines.test)
-    implementation("app.softwork:kotlinx-uuid-sqldelight:0.1.2")
-    implementation("app.softwork:kotlinx-uuid-core-jvm:0.1.2")
-    implementation("com.google.code.gson:gson:2.10.1")
-    testImplementation(kotlin("test"))
-    implementation("org.slf4j:slf4j-api:2.0.7") // Ensure you use the latest version
-    implementation("ch.qos.logback:logback-classic:1.4.11") // Ensure you use the latest version
     implementation(compose.materialIconsExtended)
-    implementation("io.coil-kt.coil3:coil-compose:3.0.0-rc01")
-    implementation("io.coil-kt.coil3:coil-network-okhttp:3.0.0-rc01")
+    implementation(libs.kotlinx.uuid.sqldelight)
+    implementation(libs.kotlinx.uuid.core.jvm)
+    implementation(libs.gson)
+    implementation(libs.slf4j.api)
+    implementation(libs.logback.classic)
+    implementation(libs.coil.compose)
+    implementation(libs.coil.network.okhttp)
+
+    testImplementation(kotlin("test"))
+    implementation(libs.kotlinx.coroutines.test)
 }
 
 sqldelight {
